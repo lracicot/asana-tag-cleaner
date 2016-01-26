3ddf4f412a5b08ac6fa52be0bd0afb2e0fc24107
Louis Racicot <info@louisracicot.com>
2016-01-26 12:51:58 -0500

    more functions

--- index.js
+++ index.js
@@ -6,44 +6,69 @@ var levenshtein = require('fast-levenshtein');
 var accessToken = process.env.ASANA_ACCESS_TOKEN;
 var workspaceId = process.env.ASANA_WORKSPACE_ID;
 
+if (!accessToken) {
+	console.error('Please add the ASANA_ACCESS_TOKEN to your env variables');
+	return 1;
+}
+
+if (!workspaceId) {
+	console.error('Please add the ASANA_WORKSPACE_ID to your env variables');
+	return 1;
+}
+
 var client = asana.Client.create().useAccessToken(accessToken);
 
+
+function getTags(workspace, client) {
+    return client.tags.findByWorkspace(workspace)
+    .then(collection => collection.fetch())
+    .then(function(tags) {
+        var tagList = [];
+
+        tags.forEach(tag => {
+            tagList.push(tag);
+        });
+
+        return tagList;
+    });
+}
+
+function findSimilarTags(tags) {
+
+	var threshold = 2;
+
+	tags.forEach(function (tag1) {
+		console.log("Tags similar to \"" + tag1.name + "\":");
+
+		tags.forEach(function (tag2) {
+			// Check the word distance
+			var l = levenshtein.get(tag1.name, tag2.name);
+
+			// Check for two inverted words
+			if (l > threshold) {
+				l = levenshtein.get(tag1.name, tag2.name.replace(/^(\w+)(\s|-|_)(\w+)$/,"$3$2$1"));
+			}
+
+			// Check for added space or separator
+			if (l > threshold) {
+				l = levenshtein.get(tag1.name, tag2.name.replace(/^(\w+)(\s|-|_)(\w+)$/,"$3$1"));
+			}
+
+			if (l <= threshold && l > 0) {
+				console.log(tag2.name);
+			}
+		});
+
+		console.log("");
+	});
+}
 
 client.users.me().then(function(me) {
 	console.log("Hello " + me.name);
 	return client.tags.findByWorkspace(workspaceId);
 })
-.then(function(response) {
-    // There may be more pages of data, we could stream or return a promise
-    // to request those here - for now, let's just return the first page
-    // of items.
-    return response.data;
- })
-.then(function(tags) {
-
-	var threshold = 2;
-
-	for (i = 0; i < tags.length; i++) {
-		console.log("Tags similar to \"" + tags[i].name + "\":");
-		for (j = 0; j < tags.length; j++) {
-			// Check the word distance
-			var l = levenshtein.get(tags[i].name, tags[j].name);
-
-			// Check for two inverted words
-			if (l > threshold) {
-				l = levenshtein.get(tags[i].name, tags[j].name.replace(/^(\w+)(\s|-|_)(\w+)$/,"$3$2$1"));
-			}
-
-			// Check for added space or separator
-			if (l > threshold) {
-				l = levenshtein.get(tags[i].name, tags[j].name.replace(/^(\w+)(\s|-|_)(\w+)$/,"$3$1"));
-			}
-
-			if (l <= threshold && l > 0) {
-				console.log(tags[j].name);
-			}
-		}
-		console.log("");
-	}
-	
+.then(function() {
+	getTags(workspaceId, client).then(function(tags) {
+		findSimilarTags(tags);
+	});
 });
