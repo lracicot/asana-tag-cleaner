4ee8ca22c605dac1224517523dcef1ee7b0ebd9f
Louis Racicot <info@louisracicot.com>
2016-01-26 00:39:52 -0500

    working example

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-var config = require('./config');
-
 var asana = require('asana');
+var levenshtein = require('fast-levenshtein');
 
 var accessToken = process.env.ASANA_ACCESS_TOKEN;
 var workspaceId = process.env.ASANA_WORKSPACE_ID;
@@ -11,9 +10,7 @@ var client = asana.Client.create().useAccessToken(accessToken);
 
 
 client.users.me().then(function(me) {
-
 	console.log("Hello " + me.name);
-
 	return client.tags.findByWorkspace(workspaceId);
 })
 .then(function(response) {
@@ -22,6 +19,31 @@ client.users.me().then(function(me) {
     // of items.
     return response.data;
  })
-.map(function(tags) {
-	console.log(tags);
+.then(function(tags) {
+
+	var threshold = 2;
+
+	for (i = 0; i < tags.length; i++) {
+		console.log("Tags similar to \"" + tags[i].name + "\":");
+		for (j = 0; j < tags.length; j++) {
+			// Check the word distance
+			var l = levenshtein.get(tags[i].name, tags[j].name);
+
+			// Check for two inverted words
+			if (l > threshold) {
+				l = levenshtein.get(tags[i].name, tags[j].name.replace(/^(\w+)(\s|-|_)(\w+)$/,"$3$2$1"));
+			}
+
+			// Check for added space or separator
+			if (l > threshold) {
+				l = levenshtein.get(tags[i].name, tags[j].name.replace(/^(\w+)(\s|-|_)(\w+)$/,"$3$1"));
+			}
+
+			if (l <= threshold && l > 0) {
+				console.log(tags[j].name);
+			}
+		}
+		console.log("");
+	}
+	
 });
